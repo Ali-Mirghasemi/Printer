943d80a93b86afc3348e4b1ce1b71f8474262b43
Ali-Mirghasemi\Elec <dragon.13761390@gmail.com>
2022-07-26 16:07:36 +0430

    Create lib

---
+++ Src/Printer.c
@@ -0,0 +1,75 @@
+#include "Printer.h"
+
+Stream_Result Print_char(Stream* stream, char c) {
+    return Stream_writeChar(stream, c);
+}
+Stream_Result Print_str(Stream* stream, const char *str) {
+    return Stream_writeStr(stream, str);
+}
+Stream_Result Print_num(Stream* stream, int num) {
+    char buf[12];
+    Str_LenType len = Str_parseNum(num, Str_Decimal, STR_NORMAL_LEN, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_numFix(Stream* stream, int num, Str_Radix base, Str_LenType len) {
+    char buf[12];
+    Str_LenType len = Str_parseNum(num, base, len, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_unum(Stream* stream, unsigned int num) {
+    char buf[12];
+    Str_LenType len = Str_parseUNum(num, Str_Decimal, STR_NORMAL_LEN, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_unumFix(Stream* stream, unsigned int num, Str_Radix base, Str_LenType len) {
+    char buf[12];
+    Str_LenType len = Str_parseNum(num, base, len, buf);
+    return Stream_writeStr(stream, buf);
+}
+
+Stream_Result Print_float(Stream* stream, float num) {
+    char buf[17];
+    Str_LenType len = Str_parseFloat(num, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_floatFix(Stream* stream, float num, Str_LenType precision) {
+    char buf[17];
+    Str_LenType len = Str_parseFloatFix(num, buf, precision);
+    return Stream_writeStr(stream, buf);
+}
+
+#if STR_ENABLE_LONG_NUMBER
+Stream_Result Print_long(Stream* stream, long num) {
+    char buf[22];
+    Str_LenType len = Str_parseLong(num, Str_Decimal, STR_NORMAL_LEN, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_longFix(Stream* stream, long num, Str_Radix base, Str_LenType len) {
+    char buf[22];
+    Str_LenType len = Str_parseLongFix(num, base, len, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_ulong(Stream* stream, unsigned long num) {
+    char buf[22];
+    Str_LenType len = Str_parseULong(num, Str_Decimal, STR_NORMAL_LEN, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_ulongFix(Stream* stream, unsigned long num, Str_Radix base, Str_LenType len) {
+    char buf[22];
+    Str_LenType len = Str_parseULongFix(num, base, len, buf);
+    return Stream_writeStr(stream, buf);
+}
+#endif
+
+#if STR_ENABLE_DOUBLE
+Stream_Result Print_double(Stream* stream, double num) {
+    char buf[33];
+    Str_LenType len = Str_parseDouble(num, buf);
+    return Stream_writeStr(stream, buf);
+}
+Stream_Result Print_doubleFix(Stream* stream, double num, Str_LenType precision) {
+    char buf[33];
+    Str_LenType len = Str_parseDoubleFix(num, buf, precision);
+    return Stream_writeStr(stream, buf);
+}
+#endif
---
+++ Src/Printer.h
@@ -0,0 +1,49 @@
+/**
+ * @file Printer.h
+ * @author Ali Mirghasemi (ali.mirghasemi1376@gmail.com)
+ * @brief This Library help you to print your data in Stream.
+ * @version 0.1
+ * @date 2022-07-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _PRINTER_H_
+#define _PRINTER_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Str.h"
+#include "StreamBuffer.h"
+
+Stream_Result Print_char(Stream* stream, char c);
+Stream_Result Print_str(Stream* stream, const char *str);
+Stream_Result Print_num(Stream* stream, int num);
+Stream_Result Print_numFix(Stream* stream, int num, Str_Radix base, Str_LenType len);
+Stream_Result Print_unum(Stream* stream, unsigned int num);
+Stream_Result Print_unumFix(Stream* stream, unsigned int num, Str_Radix base, Str_LenType len);
+
+Stream_Result Print_float(Stream* stream, float num);
+Stream_Result Print_floatFix(Stream* stream, float num, Str_LenType precision);
+
+#if STR_ENABLE_LONG_NUMBER
+    Stream_Result Print_long(Stream* stream, long num);
+    Stream_Result Print_longFix(Stream* stream, long num, Str_Radix base, Str_LenType len);
+    Stream_Result Print_ulong(Stream* stream, unsigned long num);
+    Stream_Result Print_ulongFix(Stream* stream, unsigned long num, Str_Radix base, Str_LenType len);
+#endif
+
+#if STR_ENABLE_DOUBLE
+    Stream_Result Print_double(Stream* stream, double num);
+    Stream_Result Print_doubleFix(Stream* stream, double num, Str_LenType precision);
+#endif
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // _PRINTER_H_
