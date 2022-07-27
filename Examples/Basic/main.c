2b38ffb063e52cecd3240e02dc0b13c988c70530
Ali-Mirghasemi\Elec <dragon.13761390@gmail.com>
2022-07-27 12:59:40 +0430

    Update examples

--- Examples/Basic/main.c
+++ Examples/Basic/main.c
@@ -14,8 +14,13 @@ int main()
     OStream_init(&ostream, Printer_transmit, (uint8_t*) buff, sizeof(buff));
 
 
-    Print_str(&ostream.Buffer, "[Basic Printer]\n");
-    Print_num(&ostream.Buffer, 1024);
+    Println_str(&ostream.Buffer, "[Basic Printer]");
+    Println_num(&ostream.Buffer, 1024);
+    Println_floatFix(&ostream.Buffer, 2.145f, 3);
+    Println_float(&ostream.Buffer, 2.125f);
+    Println_ulong(&ostream.Buffer, 4000000);
+    Println_numFix(&ostream.Buffer, 124, Str_Binary, 8);
+    Println_numFix(&ostream.Buffer, 0x45, Str_Hex, 8);
 
     OStream_flush(&ostream);
 }
