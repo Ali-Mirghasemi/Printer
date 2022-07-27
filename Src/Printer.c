1c78214a3580de8791500a46ee9e7150967e2fb1
Ali-Mirghasemi\Elec <dragon.13761390@gmail.com>
2022-07-27 12:59:20 +0430

    Add println functions

--- Src/Printer.c
+++ Src/Printer.c
@@ -1,5 +1,7 @@
 #include "Printer.h"
 
+static const char ENDLINE[] = PRINTER_ENDLINE;
+
 Stream_Result Print_char(Stream* stream, char c) {
     return Stream_writeChar(stream, c);
 }
@@ -73,3 +75,227 @@ Stream_Result Print_doubleFix(Stream* stream, double num, Str_LenType precision)
     return Stream_writeStr(stream, buf);
 }
 #endif
+
+/**
+ * @brief print a empty line
+ * 
+ * @param stream 
+ * @return Stream_Result 
+ */
+Stream_Result Print_line(Stream* stream) {
+    return Stream_writeBytes(stream, (uint8_t*) ENDLINE, sizeof(ENDLINE) - 1);
+}
+/**
+ * @brief print single char and end line
+ * 
+ * @param stream 
+ * @param c 
+ * @return Stream_Result 
+ */
+Stream_Result Println_char(Stream* stream, char c) {
+    Stream_Result result = Print_char(stream, c);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print string and end line
+ * 
+ * @param stream 
+ * @param str 
+ * @return Stream_Result 
+ */
+Stream_Result Println_str(Stream* stream, const char *str) {
+    Stream_Result result = Print_str(stream, str);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @return Stream_Result 
+ */
+Stream_Result Println_num(Stream* stream, int num) {
+    Stream_Result result = Print_num(stream, num);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print num and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @param base 
+ * @param minLen 
+ * @return Stream_Result 
+ */
+Stream_Result Println_numFix(Stream* stream, int num, Str_Radix base, Str_LenType minLen) {
+    Stream_Result result = Print_numFix(stream, num, base, minLen);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print unsigned number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @return Stream_Result 
+ */
+Stream_Result Println_unum(Stream* stream, unsigned int num) {
+    Stream_Result result = Print_unum(stream, num);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print unsigned number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @param base 
+ * @param minLen 
+ * @return Stream_Result 
+ */
+Stream_Result Println_unumFix(Stream* stream, unsigned int num, Str_Radix base, Str_LenType minLen) {
+    Stream_Result result = Print_unumFix(stream, num, base, minLen);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print float and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @return Stream_Result 
+ */
+Stream_Result Println_float(Stream* stream, float num) {
+    Stream_Result result = Print_float(stream, num);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print float and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @param precision 
+ * @return Stream_Result 
+ */
+Stream_Result Println_floatFix(Stream* stream, float num, Str_LenType precision) {
+    Stream_Result result = Print_floatFix(stream, num, precision);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+
+#if STR_ENABLE_LONG_NUMBER
+/**
+ * @brief print long number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @return Stream_Result 
+ */
+Stream_Result Println_long(Stream* stream, long num) {
+    Stream_Result result = Print_long(stream, num);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print long number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @param base 
+ * @param minLen 
+ * @return Stream_Result 
+ */
+Stream_Result Println_longFix(Stream* stream, long num, Str_Radix base, Str_LenType minLen) {
+    Stream_Result result = Print_longFix(stream, num, base, minLen);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print unsigned long number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @return Stream_Result 
+ */
+Stream_Result Println_ulong(Stream* stream, unsigned long num) {
+    Stream_Result result = Print_ulong(stream, num);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print unsigned long number and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @param base 
+ * @param minLen 
+ * @return Stream_Result 
+ */
+Stream_Result Println_ulongFix(Stream* stream, unsigned long num, Str_Radix base, Str_LenType minLen) {
+    Stream_Result result = Print_ulongFix(stream, num, base, minLen);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+#endif
+
+#if STR_ENABLE_DOUBLE
+/**
+ * @brief print double and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @return Stream_Result 
+ */
+Stream_Result Println_double(Stream* stream, double num) {
+    Stream_Result result = Print_double(stream, num);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+/**
+ * @brief print double and end line
+ * 
+ * @param stream 
+ * @param num 
+ * @param precision 
+ * @return Stream_Result 
+ */
+Stream_Result Println_doubleFix(Stream* stream, double num, Str_LenType precision) {
+    Stream_Result result = Print_doubleFix(stream, num, precision);
+    if (result == Str_Ok) {
+        result = Print_line(stream);
+    }
+    return result;
+}
+#endif
+
+
--- Src/Printer.h
+++ Src/Printer.h
@@ -17,6 +17,9 @@
 extern "C" {
 #endif
 
+#define PRINTER_ENDLINE                     "\r\n"
+
+
 #include "Str.h"
 #include "StreamBuffer.h"
 
@@ -41,6 +44,31 @@ Stream_Result Print_floatFix(Stream* stream, float num, Str_LenType precision);
     Stream_Result Print_double(Stream* stream, double num);
     Stream_Result Print_doubleFix(Stream* stream, double num, Str_LenType precision);
 #endif
+
+Stream_Result Print_line(Stream* stream);
+
+Stream_Result Println_char(Stream* stream, char c);
+Stream_Result Println_str(Stream* stream, const char *str);
+Stream_Result Println_num(Stream* stream, int num);
+Stream_Result Println_numFix(Stream* stream, int num, Str_Radix base, Str_LenType minLen);
+Stream_Result Println_unum(Stream* stream, unsigned int num);
+Stream_Result Println_unumFix(Stream* stream, unsigned int num, Str_Radix base, Str_LenType minLen);
+
+Stream_Result Println_float(Stream* stream, float num);
+Stream_Result Println_floatFix(Stream* stream, float num, Str_LenType precision);
+
+#if STR_ENABLE_LONG_NUMBER
+    Stream_Result Println_long(Stream* stream, long num);
+    Stream_Result Println_longFix(Stream* stream, long num, Str_Radix base, Str_LenType minLen);
+    Stream_Result Println_ulong(Stream* stream, unsigned long num);
+    Stream_Result Println_ulongFix(Stream* stream, unsigned long num, Str_Radix base, Str_LenType minLen);
+#endif
+
+#if STR_ENABLE_DOUBLE
+    Stream_Result Println_double(Stream* stream, double num);
+    Stream_Result Println_doubleFix(Stream* stream, double num, Str_LenType precision);
+#endif
+
 
 #ifdef __cplusplus
 };
